edb99e831be02ac4c36062c4159afabdde2256c6
Alex-Guima <alexandreagtaveira@gmail.com>
2026-03-08 13:31:18 -0300

    Implement task management features with exception handling and REST API endpoints

--- src/main/java/com/alex/guima/domain/entity/interfaces/Completable.java
+++
@@ -1,6 +0,0 @@
-package com.alex.guima.domain.entity.interfaces;
-
-public interface Completable {
-    boolean isCompleted();
-    void setCompleted(boolean completed);
-}
--- src/main/java/com/alex/guima/repository/TaskRepository.java
+++ src/main/java/com/alex/guima/repository/TaskRepository.java
@@ -1,25 +1,12 @@
 package com.alex.guima.repository;
 
-import java.util.List;
-
 import com.alex.guima.domain.entity.Task;
-
-import io.quarkus.panache.common.Page;
+import io.quarkus.hibernate.reactive.panache.PanacheRepository;
 import io.smallrye.mutiny.Uni;
-import jakarta.enterprise.context.RequestScoped;
+import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.ws.rs.NotFoundException;
+import org.jspecify.annotations.NonNull;
 
-@RequestScoped
-public class TaskRepository {
-    public Uni<List<Task>> findAll() {
-        return Task.findAll().page(Page.ofSize(20)).list();
-    }
-
-    public Uni<Task> findById(Long id) {
-        return Task.findById(id).onItem().ifNull().failWith(NotFoundException::new).onItem().transform(Task.class::cast);
-    }
-
-    public Uni<Task> persist(Task task) {
-        return task.persist();
-    }
+@ApplicationScoped
+public class TaskRepository implements PanacheRepository<Task> {
 }
