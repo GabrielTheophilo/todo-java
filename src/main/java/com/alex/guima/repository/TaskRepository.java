4d0977d7c7a96b83bcbb60447d01d289cbb040fb
Alex-Guima <alexandreagtaveira@gmail.com>
2026-03-08 13:32:00 -0300

    Removed unused imports

--- src/main/java/com/alex/guima/repository/TaskRepository.java
+++ src/main/java/com/alex/guima/repository/TaskRepository.java
@@ -2,10 +2,7 @@ package com.alex.guima.repository;
 
 import com.alex.guima.domain.entity.Task;
 import io.quarkus.hibernate.reactive.panache.PanacheRepository;
-import io.smallrye.mutiny.Uni;
 import jakarta.enterprise.context.ApplicationScoped;
-import jakarta.ws.rs.NotFoundException;
-import org.jspecify.annotations.NonNull;
 
 @ApplicationScoped
 public class TaskRepository implements PanacheRepository<Task> {
